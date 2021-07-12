84c08d6b9ff68b3778f14b3457d909dba1c774e4
dani Kim <rlaeksl0124@naver.com>
2021-07-13 02:14:41 +0900

    Brush size & Filling

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 700; // 캔버스 사이즈 설정
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c"; // 선색적용: 검정
+canvas.width = CANVAS_SIZE; // 캔버스 사이즈 설정
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR; // 선색적용: 검정
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5; // 선의 두께
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -34,6 +41,28 @@ function onMouseMove(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "채우기";
+  } else {
+    filling = true;
+    mode.innerText = "그리기";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // 좌표 x, y, 크기 가로세로
+  }
 }
 
 if (canvas) {
@@ -42,8 +71,17 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting); // 마우스클릭시 이벤트발생
   canvas.addEventListener("mouseup", stopPainting); // 마우스를 뗄때 이벤트 발생
   canvas.addEventListener("mouseleave", stopPainting); // 마우스가 canvas를 떠날때
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 ); // colors를 array로 만들고 each문을 돌린다
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+} // input의 값에따라 반응하는 이벤트
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
